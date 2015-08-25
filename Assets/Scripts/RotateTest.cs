0291731753681dd56f3cde3dd6d6a297fe0df41e
britg <brit@britg.com>
2015-08-25 07:51:33 -0500

    work on rendering the game world

---
+++ Assets/Scripts/RotateTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTest : MonoBehaviour {
+
+  public float mult = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.localEulerAngles = transform.localEulerAngles + mult * Vector3.up * Time.deltaTime;
+	}
+}
