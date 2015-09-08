9a5d12f29f311220cf932f50efb3208774cf8f52
britg <brit@britg.com>
2015-09-07 21:02:12 -0500

    Convert TileType array to a list of Tiles

---
+++ Assets/Scripts/Lib/DunGen/Tile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DunGen {
+  public class Tile {
+
+    public enum Type {
+      Nothing,
+      Blocked,
+      Room,
+      Corridor,
+      Perimeter,
+      Entrance,
+      Door,
+      StairUp,
+      StairDown
+    }
+
+    public Type type;
+    public Vector3 position;
+  }
+}
